b5075705033c8b7d543f6213f8cc3011b8175eae
CJSCelebrity <sewcharrancaleb@gmail.com>
2026-09-13 17:40:16 +0200

    Added in the validation rules for the CreateShowBookingValidator.cs and the CreateVehicleBookingValidator.cs as these validations were missing causing issues when the dependency is loaded

--- WBS_Assessment.Application/Validators/CreateShowBookingValidator.cs
+++ WBS_Assessment.Application/Validators/CreateShowBookingValidator.cs
@@ -1,6 +1,14 @@
-﻿namespace WBS_Assessment.Application.Validators;
+﻿using FluentValidation;
+using WBS_Assessment.Application.Dto;
 
-public class CreateShowBookingValidator
+namespace WBS_Assessment.Application.Validators;
+
+public class CreateShowBookingValidator : AbstractValidator<CreateShowBookingRequest>
 {
-    
+    public CreateShowBookingValidator()
+    {
+        RuleFor(x => x.UserId).NotEmpty();
+        RuleFor(x => x.ItemId).NotEmpty();
+        RuleFor(x => x.PerformanceTime).NotEmpty();
+    }
 }
--- WBS_Assessment.Application/Validators/CreateVehicleBookingValidator.cs
+++ WBS_Assessment.Application/Validators/CreateVehicleBookingValidator.cs
@@ -1,6 +1,14 @@
-﻿namespace WBS_Assessment.Application.Validators;
+﻿using FluentValidation;
+using WBS_Assessment.Application.Dto;
 
-public class CreateVehicleBookingValidator
+namespace WBS_Assessment.Application.Validators;
+
+public class CreateVehicleBookingValidator : AbstractValidator<CreateVehicleBookingRequest>
 {
-    
+    public CreateVehicleBookingValidator()
+    {
+        RuleFor(x => x.UserId).NotEmpty();
+        RuleFor(x => x.ItemId).NotEmpty();
+        RuleFor(x => x.Dropoff).GreaterThan(x => x.Pickup);
+    }
 }
