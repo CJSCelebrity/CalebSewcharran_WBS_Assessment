bb75dd9893648ea16f7b0adbb417cd282c9bd632
CJSCelebrity <sewcharrancaleb@gmail.com>
2026-09-13 17:34:47 +0200

    Updated the ApplicationServiceRegistration.cs to have the Validator dependency as a Singleton service

--- WBS_Assessment.Application/ServiceRegistration/ApplicationServiceRegistration.cs
+++ WBS_Assessment.Application/ServiceRegistration/ApplicationServiceRegistration.cs
@@ -10,9 +10,7 @@ public static class ApplicationServiceRegistration
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddValidatorsFromAssemblyContaining<CreateApartmentBookingValidator>();
-        services.AddValidatorsFromAssemblyContaining<CreateShowBookingValidator>();
-        services.AddValidatorsFromAssemblyContaining<CreateVehicleBookingValidator>();
+        services.AddValidatorsFromAssemblyContaining<CreateApartmentBookingValidator>(lifetime: ServiceLifetime.Singleton);
         
         services.AddSingleton<BookingGuard>();
         services.AddSingleton<ApartmentBookingService>();
